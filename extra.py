f091c0798398ecd5028cddd271cbf37fac16b328
ce21plozz <ceplox021@gmail.com>
2025-10-23 10:31:53 +0700

    Update extra.py

---
+++ extra.py
@@ -0,0 +1,110 @@
+import streamlit as st
+import base64
+def get_base64(file_path):
+    with open(file_path, "rb") as f:
+        return base64.b64encode(f.read()).decode()
+ekstra1 = get_base64("image.jpg")
+
+st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: linear-gradient(0deg, #242424, #3d3d3d);
+        background-position: center top;
+        background-attachment: fixed;
+        background-size: cover;
+        background-repeat: no-repeat;
+    }}
+    </style>
+    """, unsafe_allow_html=True)
+st.html(f"""
+        <div class="container">
+            <h1>EKSTRA</h1>
+            <hr style="background-color: white;margin:25px;">
+            <img class="img1" src="data:image/jpg;base64,{ekstra1}">
+            <div class="title">Tempat Duduk Generator</div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias consequatur doloremque doloribus illo ipsam minus quae repudiandae. Amet consequatur deleniti eum inventore, laudantium neque quis quod repellendus soluta veniam.</p>
+            <span class="link1"><a href="https://layout-tempat-duduk-generator.streamlit.app/" target="_blank">Kunjungi situsnya</a></span>
+        <hr style="background-color: white;margin:25px;">
+            <img class="img2" src="data:image/jpg;base64,{ekstra1}">
+            <div class="title">Kelompok Generator</div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A alias consequatur doloremque doloribus illo ipsam minus quae repudiandae. Amet consequatur deleniti eum inventore, laudantium neque quis quod repellendus soluta veniam.</p>
+            <span class="link2"><a href="https://kelompok.streamlit.app/" target="_blank">Kunjungi situsnya</a></span>
+        </div>
+        """)
+st.html("""
+        <style>
+        h1 {
+        color: white;
+        font-size: 3em;
+        }
+        
+        p {
+        color: white;
+        }
+        
+        .container {
+        text-align: center;
+        margin: auto;
+        }
+        
+        img {
+        width: 175px;
+        box-sizing: border-box;
+        padding: 6px;
+        border-radius: 12px;
+        animation : glow 2s ease infinite;
+        }
+        .img1 {
+        background-image: linear-gradient(120deg,#23c902, #db221f, #49dbde, #fcba03);
+        animation-delay: 0.33s;
+        }
+        .img2 {
+        background-image: linear-gradient(188deg,#1f4edb, #db451f, #b2db1f, #db1f74);
+        animation-delay: 0.95s;
+        }
+        
+        .link1 {
+        border-radius: 50px;
+        border: 3px solid black;
+        background-color: #f5d671;
+        color: white;
+        padding: 10px;
+        display: inline-block;
+        width: 250px;
+        animation: colorchange 3s linear infinite;
+        }
+        .link2 {
+        border-radius: 50px;
+        border: 3px solid black;
+        background-color: #f5d671;
+        color: white;
+        padding: 10px;
+        display: inline-block;
+        width: 250px;
+        animation: colorchange 3s linear infinite;
+        animation-delay: 0.45s;
+        }
+        
+        a {
+        text-decoration: none;
+        font-weight: bold;
+        }
+        
+        .title {
+        font-size: 2.1em;
+        color: white;
+        }
+        
+        @keyframes glow {
+            25%{box-shadow: 0px 0px 40px #4b68eb}
+            50%{box-shadow: 0px 0px 40px yellow}
+            75%{box-shadow: 0px 0px 40px #a655ed}
+        }
+        @keyframes colorchange{
+            25%{background-color: #f57171}
+            50%{background-color: #82f571}
+            75%{background-color: #71cdf5}
+        }
+        </style>
+        """)
